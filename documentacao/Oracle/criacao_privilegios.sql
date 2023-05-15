7b145fa5389b976032bb48f32ee70ff99956ed9a
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2023-05-15 14:20:56 -0300

    ok

--- documentacao/Oracle/criacao_privilegios.sql
+++ documentacao/Oracle/criacao_privilegios.sql
@@ -1,16 +1,16 @@
-CREATE USER inspecao_sesmt IDENTIFIED BY dick_vigarista_2023_canguru;
+CREATE USER controle_chave IDENTIFIED BY mangan_ti_usiuso_nga_asno_2023;
 
-GRANT CREATE SESSION TO inspecao_sesmt;
-GRANT CREATE PROCEDURE TO inspecao_sesmt;
-GRANT CREATE TABLE TO inspecao_sesmt;
-GRANT CREATE VIEW TO inspecao_sesmt;
-GRANT UNLIMITED TABLESPACE TO inspecao_sesmt;
-GRANT CREATE SEQUENCE TO inspecao_sesmt;
+GRANT CREATE SESSION TO controle_chave;
+GRANT CREATE PROCEDURE TO controle_chave;
+GRANT CREATE TABLE TO controle_chave;
+GRANT CREATE VIEW TO controle_chave;
+GRANT UNLIMITED TABLESPACE TO controle_chave;
+GRANT CREATE SEQUENCE TO controle_chave;
 
-GRANT SELECT ON portal_projetos.SEQ_CD_ACESSO TO inspecao_sesmt;
-GRANT INSERT ON portal_projetos.ACESSO TO inspecao_sesmt;
+GRANT SELECT ON portal_projetos.SEQ_CD_ACESSO TO controle_chave;
+GRANT INSERT ON portal_projetos.ACESSO TO controle_chave;
 
-GRANT EXECUTE ON dbasgu.FNC_MV2000_HMVPEP TO inspecao_sesmt;
+GRANT EXECUTE ON dbasgu.FNC_MV2000_HMVPEP TO controle_chave;
 
-GRANT SELECT ON dbasgu.USUARIOS TO inspecao_sesmt;
-GRANT SELECT ON dbasgu.PAPEL_USUARIOS TO inspecao_sesmt;
+GRANT SELECT ON dbasgu.USUARIOS TO controle_chave;
+GRANT SELECT ON dbasgu.PAPEL_USUARIOS TO controle_chave;
